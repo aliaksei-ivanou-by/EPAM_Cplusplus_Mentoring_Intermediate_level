57a5f26f6a83b6aa2a4dbacf00bd00a4ff0263c4
Aliaksei Ivanou <aliaksei.ivanou.by@icloud.com>
2024-08-12 12:43:44 +0300

    Update Task 01

--- 01_multithreading_in_cplusplus_optional.cpp
+++ 01_multithreading_in_cplusplus_optional.cpp
@@ -1,174 +1,168 @@
 // 01_multithreading_in_cplusplus_optional.cpp
 
+#include <atomic>
+#include <condition_variable>
+#include <cstdarg>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
 #include <mutex>
+#include <string>
 #include <thread>
 #include <vector>
 #include <queue>
 
 enum class Errors
 {
-    // TODO: Exception handling needs to be done
     WrongParameters = 1,
     TargetFileExists,
     ErrorOpeningFile
 };
 
+void sendErrorMessage(const char* format, ...)
+{
+    static std::mutex cerr_mutex;
+    std::lock_guard<std::mutex> lock_cerr(cerr_mutex);
+    try
+    {
+        va_list args_format;
+        va_start(args_format, format);
+        va_list args;
+        va_copy(args, args_format);
+
+        const int length = std::vsnprintf(nullptr, 0, format, args_format);
+        va_end(args_format);
+
+        std::vector<char> buf(length + 1);
+        std::vsnprintf(buf.data(), buf.size(), format, args);
+        va_end(args);
+
+        std::cerr << "Error: " << buf.data() << "\n";
+    }
+    catch (const std::exception& error)
+    {
+        std::cerr << "Formatting error: " << error.what() << "\n";
+    }
+}
+
 constexpr std::size_t BUFFER_SIZE = 1024 * 1024;
-std::mutex cerr_mutex;
-std::mutex mutex;
+constexpr std::size_t MAX_BLOCKS = 4;
 
 class CopyQueue
 {
 public:
-    CopyQueue() : m_end(false)
-    {}
-    CopyQueue(std::ifstream& src) : m_end(false)
+    CopyQueue()
     {
-        src.seekg(0, src.end);
-        m_file_size = src.tellg();
-        m_file_current_size = src.tellg();
-        src.seekg(0, src.beg);
+        for (std::size_t i = 0; i < MAX_BLOCKS; ++i)
+        {
+            m_availableBuffers.emplace(std::vector<char>(BUFFER_SIZE));
+        }
     }
-    void add_to_queue(std::vector<char> values)
-    {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_queue.push(values);
-        m_cond.notify_one();
-    }
-    bool remove_from_queue(std::vector<char>& values)
+    void add_buffer_to_write_queue(std::vector<char> values, std::size_t size)
     {
         std::unique_lock<std::mutex> lock(m_mutex);
-        while (m_queue.empty() && !m_end)
-            m_cond.wait(lock);
-        if (m_queue.empty())
-            return false;
-        values = m_queue.front();
-        m_queue.pop();
-        return true;
+        m_queueToWrite.push(std::make_pair(std::move(values), size));
+        m_cond_empty.notify_one();
     }
-    void end()
+    std::pair<std::vector<char>, std::size_t> get_buffer_from_write_queue()
     {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_end = true;
-        m_cond.notify_all();
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_cond_empty.wait(lock, [this]
+            {
+                return !m_queueToWrite.empty();
+            });
+        std::pair<std::vector<char>, std::size_t> buffer = std::move(m_queueToWrite.front());
+        m_queueToWrite.pop();
+        m_cond_full.notify_one();
+        return buffer;
     }
-    size_t get_file_size()
+    std::vector<char> get_available_buffer()
     {
-        return m_file_size;
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_cond_full.wait(lock, [this]
+            {
+                return !m_availableBuffers.empty();
+            });
+        std::vector<char> buffer = std::move(m_availableBuffers.front());
+        m_availableBuffers.pop();
+        return buffer;
     }
-    int get_file_current_size()
+    void return_available_buffer(std::vector<char> buffer)
     {
-        return m_file_current_size;
-    }
-    void set_file_current_size(size_t value)
-    {
-        m_file_current_size = value;
-    }
-    void set_fileparts(int value)
-    {
-        m_fileparts = value;
-    }
-    int get_fileparts()
-    {
-        return m_fileparts;
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_availableBuffers.push(std::move(buffer));
+        m_cond_full.notify_one();
     }
 private:
-    std::queue<std::vector<char>> m_queue;
+    std::queue<std::pair<std::vector<char>, std::size_t>> m_queueToWrite;
+    std::queue<std::vector<char>> m_availableBuffers;
     std::mutex m_mutex;
-    std::condition_variable m_cond;
-    std::atomic<bool> m_end;
-    std::atomic<int> m_fileparts;
-    int m_file_size;
-    int m_file_current_size;
+    std::condition_variable m_cond_empty;
+    std::condition_variable m_cond_full;
 };
 
-void reader(std::ifstream& src, CopyQueue& queue, size_t buffer_size)
+void reader(std::ifstream& src, CopyQueue& queue)
 {
-    std::lock_guard<std::mutex> lock(mutex); // It's needed to fix parallel work
-    while (!src.eof() && queue.get_fileparts() > 0)
+    while (!src.eof())
     {
-        std::vector<char> buffer(buffer_size);
-        src.read(buffer.data(), buffer_size);
-        if (src.gcount() < buffer_size)
-            buffer.resize(src.gcount());
-        queue.set_file_current_size(queue.get_file_current_size() - buffer.size());
-        queue.set_fileparts(queue.get_fileparts() - 1);
-        queue.add_to_queue(buffer);
-        if (queue.get_fileparts() <= 0)
-            break;
+        std::vector<char> buffer = queue.get_available_buffer();
+        src.read(buffer.data(), BUFFER_SIZE);
+        queue.add_buffer_to_write_queue(std::move(buffer), src.gcount());
     }
-    queue.end();
+    queue.add_buffer_to_write_queue(std::vector<char>(), 0);
 }
 
 void writer(std::ofstream& trgt, CopyQueue& queue)
 {
-    std::vector<char> buffer;
-    while (queue.remove_from_queue(buffer))
-        trgt.write(buffer.data(), buffer.size());
+    while (true)
+    {
+        auto [values, size] = queue.get_buffer_from_write_queue();
+        if (values.empty())
+            break;
+        trgt.write(values.data(), size);
+        queue.return_available_buffer(std::move(values));
+    }
 }
 
 int main(const int argc, char* argv[])
 {
     if (argc != 4)
     {
-        std::unique_lock<std::mutex> lock_cerr(cerr_mutex);
-        std::cerr << "Parameters of the function " << argv[0] << " should be: <source> <target> <overwrite_flag>.\n";
-        lock_cerr.unlock();
+        sendErrorMessage("Parameters of the function %s should be: <source> <target> <overwrite_flag>.", argv[0]);
         return static_cast<int>(Errors::WrongParameters);
     }
 
     std::string source = argv[1];
     std::string target = argv[2];
     bool overwrite_flag = std::stoi(argv[3]);
-    std::size_t num_threads = std::thread::hardware_concurrency();
 
     if (std::filesystem::exists(target) && !overwrite_flag)
     {
-        std::unique_lock<std::mutex> lock_cerr(cerr_mutex);
-        std::cerr << "Target file exists. To overwrite, set the overwrite flag to 1.\n";
-        lock_cerr.unlock();
+        sendErrorMessage("Target file exists. To overwrite, set the overwrite flag to 1.");
         return static_cast<int>(Errors::TargetFileExists);
     }
 
     std::ifstream src(source, std::ios::binary);
     if (!src)
     {
-        std::unique_lock<std::mutex> lock_cerr(cerr_mutex);
-        std::cerr << "Error with opening source file.\n";
-        lock_cerr.unlock();
+        sendErrorMessage("Error with opening source file.");
         return static_cast<int>(Errors::ErrorOpeningFile);
     }
 
-    std::ofstream trgt(target, std::ios::binary | std::ios::app);
+    std::ofstream trgt(target, std::ios::binary | std::ios::trunc);
     if (!trgt)
     {
-        std::unique_lock<std::mutex> lock_cerr(cerr_mutex);
-        std::cerr << "Error with opening target file.\n";
-        lock_cerr.unlock();
+        sendErrorMessage("Error with opening target file.");
         return static_cast<int>(Errors::ErrorOpeningFile);
     }
 
-    CopyQueue copy_queue(src);
-    copy_queue.set_fileparts(std::ceil(static_cast<double>(copy_queue.get_file_size()) / BUFFER_SIZE));
+    CopyQueue copy_queue;
 
-    std::vector<std::thread> read_threads;
-    for (size_t i = 0; i < num_threads; ++i)
-        read_threads.emplace_back(reader, std::ref(src), std::ref(copy_queue), BUFFER_SIZE);
+    std::thread reading_thread(reader, std::ref(src), std::ref(copy_queue));
+    std::thread writing_thread(writer, std::ref(trgt), std::ref(copy_queue));
 
-    std::vector<std::thread> write_threads;
-    for (size_t i = 0; i < num_threads; ++i)
-        write_threads.emplace_back(writer, std::ref(trgt), std::ref(copy_queue));
-
-    for (auto& rt : read_threads)
-        rt.join();
-
-    copy_queue.end();
-
-    for (auto& wt : write_threads)
-        wt.join();
+    reading_thread.join();
+    writing_thread.join();
 
     return 0;
 }
